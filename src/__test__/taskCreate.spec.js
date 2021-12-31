bfec5b5d070f473bc88a11138de4aa3f890d3f42
yanmifeakeju <yanmifeakeju@gmail.com>
2021-12-31 09:18:22 +0100

    Update Task Route: authenticated request with valid data return status code of 200.

--- src/__test__/taskCreate.spec.js
+++ src/__test__/taskCreate.spec.js
@@ -23,7 +23,7 @@ describe('Task Creation', () => {
     expect(response.status).toBe(401);
   });
 
-  it('returns 400 for authenticated request with empty fields and values', async () => {
+  it('returns 400 for authenticated request with empty fields', async () => {
     const user = await createUser(validData);
     const token = await user.generateAuthToken();
 
@@ -33,5 +33,76 @@ describe('Task Creation', () => {
 
     expect(response.status).toBe(400);
     expect(response.body.data.validationErrors).toBeTruthy();
+  });
+
+  it('returns 400 for authenticated request with invalid fields', async () => {
+    const user = await createUser(validData);
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post('/api/v1/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'random name', lastname: 'random lastname' });
+
+    expect(response.status).toBe(400);
+    expect(response.body.data.validationErrors).toBeTruthy();
+  });
+
+  it('returns 400 for authenticated request with valid fields but invalid values', async () => {
+    const user = await createUser(validData);
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post('/api/v1/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ title: 40, description: 50 });
+
+    expect(response.status).toBe(400);
+    expect(response.body.data.validationErrors).toBeTruthy();
+  });
+
+  it('returns 400 for authenticated request with valid fields and valid values, but invlaid value for optional field', async () => {
+    const user = await createUser(validData);
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post('/api/v1/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'a title',
+        description: 'a description',
+        priority: 'mademan',
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body.data.validationErrors).toBeTruthy();
+  });
+
+  it('returns 200 for authenticated request with valid fields and valid values', async () => {
+    const user = await createUser(validData);
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post('/api/v1/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ title: 'a title', description: 'a description' });
+
+    expect(response.status).toBe(200);
+  });
+
+  it('returns 200 for authenticated request with valid fields and valid values and valid value for optional field', async () => {
+    const user = await createUser(validData);
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post('/api/v1/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'a title',
+        description: 'a description',
+        priority: 'low',
+      });
+
+    expect(response.status).toBe(200);
   });
 });
